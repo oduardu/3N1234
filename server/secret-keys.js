812014684678916267768ea4465f79241336f9df
Eduardo Pazzini Zancanaro <46853580+oduardu@users.noreply.github.com>
2024-05-05 19:14:08 -0300

    🧨 remove: secret-keys.js

--- server/secret-keys.js
+++
@@ -1,8 +0,0 @@
-const pgp = require("pg-promise")({});
-const usuario = "postgres";
-const senha = "230205";
-const host = "localhost";
-const porta = "5432";
-const bancoDeDados = "uffs";
-module.exports = pgp(`postgres://${usuario}:${senha}@${host}:${porta}/${bancoDeDados}`);
-
