40d73df42de9512f09b14ffb6871c0ae930bc882
Eduardo P Zancanaro <eduardo.zancanaro@estudante.uffs.edu.br>
2024-05-26 18:48:16 -0300

    :ok_hand: add array of curso info

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,93 +2,67 @@ import React from 'react';
 import './Info.css';
 
 function Info() {
+  const cursoInfo = [
+    { id: 'curso-name', value: 'Ciência da Computação' },
+    { id: 'turno', value: 'Noturno' },
+    { id: 'campus', value: 'Campus Chapecó' },
+    { id: 'coordenacao', value: 'Coordenação: Prof. Dr. Fulano de Tal' },
+    { id: 'descricao', value: 'Descrição do curso de Ciência da Computação.' }
+  ];
+
+  const ccrsInfo = [
+    { semestre: '1º Semestre', disciplinas: [
+      { dia: 'Segunda-feira', horario: '19:00 - 22:00', disciplina: 'Algoritmos e Estruturas de Dados' },
+      { dia: 'Terça-feira', horario: '19:00 - 22:00', disciplina: 'Redes de Computadores' },
+      { dia: 'Quarta-feira', horario: '19:00 - 22:00', disciplina: 'Banco de Dados' },
+      { dia: 'Quinta-feira', horario: '19:00 - 22:00', disciplina: 'Programação Orientada a Objetos' },
+      { dia: 'Sexta-feira', horario: '19:00 - 22:00', disciplina: 'Engenharia de Software' }
+    ] },
+    { semestre: '2º Semestre', disciplinas: [
+      { dia: 'Segunda-feira', horario: '19:00 - 22:00', disciplina: 'Algoritmos e Estruturas de Dados' },
+      { dia: 'Terça-feira', horario: '19:00 - 22:00', disciplina: 'Redes de Computadores' },
+      { dia: 'Quarta-feira', horario: '19:00 - 22:00', disciplina: 'Banco de Dados' },
+      { dia: 'Quinta-feira', horario: '19:00 - 22:00', disciplina: 'Programação Orientada a Objetos' },
+      { dia: 'Sexta-feira', horario: '19:00 - 22:00', disciplina: 'Engenharia de Software' }
+    ] }
+  ];
+
   return (
       <section className="container-main">
         <div className="curso-header">
-          <h2 id="curso-name">Ciência da Computação</h2>
+            <h2>Ciência da Computação</h2>
         </div>
         <div className="container-info">
           <div className="curso-info">
-            <span className="turno">Noturno</span>
-            <span id="campus">Campus Chapecó</span>
-            <span id="coordenacao">Coordenação: Prof. Dr. Fulano de Tal</span>
-            <p id="descricao">Descrição do curso de Ciência da Computação.</p>
+            {cursoInfo.slice(1).map(info => (
+              <span key={info.id} id={info.id}>{info.value}</span>
+            ))}
+            <p className="descricao">{cursoInfo.find(info => info.id === 'descricao').value}</p>
           </div>
           <div className="ccrs">
-            <h3>1º Semestre</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>Dia</th>
-                  <th>Horário</th>
-                  <th>Disciplina</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Segunda-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Algoritmos e Estruturas de Dados</td>
-                </tr>
-                <tr>
-                  <td>Terça-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Redes de Computadores</td>
-                </tr>
-                <tr>
-                  <td>Quarta-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Banco de Dados</td>
-                </tr>
-                <tr>
-                  <td>Quinta-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Programação Orientada a Objetos</td>
-                </tr>
-                <tr>
-                  <td>Sexta-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Engenharia de Software</td>
-                </tr>
-              </tbody>
-            </table>
-            <h3>2º Semestre</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>Dia</th>
-                  <th>Horário</th>
-                  <th>Disciplina</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Segunda-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Algoritmos e Estruturas de Dados</td>
-                </tr>
-                <tr>
-                  <td>Terça-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Redes de Computadores</td>
-                </tr>
-                <tr>
-                  <td>Quarta-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Banco de Dados</td>
-                </tr>
-                <tr>
-                  <td>Quinta-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Programação Orientada a Objetos</td>
-                </tr>
-                <tr>
-                  <td>Sexta-feira</td>
-                  <td>19:00 - 22:00</td>
-                  <td>Engenharia de Software</td>
-                </tr>
-              </tbody>
-            </table>
+            {ccrsInfo.map(semestre => (
+              <div key={semestre.semestre}>
+                <h3>{semestre.semestre}</h3>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Dia</th>
+                      <th>Horário</th>
+                      <th>Disciplina</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {semestre.disciplinas.map(disciplina => (
+                      <tr key={disciplina.dia}>
+                        <td>{disciplina.dia}</td>
+                        <td>{disciplina.horario}</td>
+                        <td>{disciplina.disciplina}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ))}
           </div>
         </div>
       </section>
@@ -96,4 +70,3 @@ function Info() {
 }
 
 export default Info;
-
